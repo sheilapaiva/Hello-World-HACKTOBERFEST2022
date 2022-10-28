eec708e03e0a3ddc92b7ed637212d612af6bf17a
Luiz Henrique Serafim <luizhserafim2001@hotmail.com>
2022-10-28 20:39:28 -0300

    Adicionei os arquivos na pasta: Hellow World Luiz Henrique

---
+++ .Hellow Word Luiz Henrique/Hellow World- Luiz Henrique.cs
@@ -0,0 +1,12 @@
+using System;
+  
+namespace Hello_World {
+      
+    class program {
+        
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
